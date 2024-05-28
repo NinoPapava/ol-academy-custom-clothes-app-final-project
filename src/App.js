4cc889af43220816309239514241753295859b48
NinoPapava <gv.papava@gmail.com>
2024-05-28 11:57:42 +0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home"
 import { Create } from "./components/Create/Create";
 import { History } from "./components/History/History";
-import { NotFound } from "./NotFound/NotFound";
+import { NotFound } from "./components/NotFound/NotFound";
 
 function App() {
   return (
